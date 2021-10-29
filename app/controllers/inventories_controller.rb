ee5cafa5826f79bb41b2252b875b1ff048e9f3ed
Muskaan Guleria <muskanguleria@IN-AP00787-MBP.local>
2021-10-29 16:29:32 +0530

    First Commit

--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -1,26 +1,24 @@
 class InventoriesController < ApplicationController
     def index
         @inventories = User.find_by(id: session[:user_id]).inventory
-        # session[:inventory_id] =  Inventory.select(:id).find_or_create_by(user_id: session[:user_id])
-        # User.find_or_create_by(id: session[:user_id]).inventory
     end
     
-    def new
-        @inventory = Inventory.new
-    end
+    # def new
+    #     @inventory = Inventory.new
+    # end
 
-    def create
-        @inventory = Inventory.new(inventory_params) 
-        if @inventory.save
-            user_id = session[:user_id]
-            redirect_to root_path, notice: "Successfully created inventory"
-        else
-            render :new
-        end
-    end
-    private
-    def inventory_params
-        params.require(:inventory).permit(:user_id)
-    end
+    # def create
+    #     @inventory = Inventory.new(inventory_params) 
+    #     if @inventory.save
+    #         user_id = session[:user_id]
+    #         redirect_to root_path, notice: "Successfully created inventory"
+    #     else
+    #         render :new
+    #     end
+    # end
+    # private
+    # def inventory_params
+    #     params.require(:inventory).permit(:user_id)
+    # end
 
 end
