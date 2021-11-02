e550ee04d5b3a02ab854cf521e2c001dd40adf2e
Muskaan Guleria <muskanguleria@IN-AP00787-MBP.local>
2021-11-03 00:19:28 +0530

    First Commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,16 @@
 class User < ActiveRecord::Base
     has_one :inventory
-    # has_many :items, through: :inventories
 
     has_secure_password
 
+    PASSWORD_REQUIREMENTS = /\A
+        (?=.{8,}) # atleast 8 characters long
+        (?=.*\d) # atleast one number
+        (?=.*[a-z]) # atleast one lowercase letter
+        (?=.*[A-Z]) # atleast one uppercase letter
+        (?=.*[[:^alnum:]]) # atleast one symbol
+    /x
+
     validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "must be a valid email address" }
+    validates :password, format: { with: PASSWORD_REQUIREMENTS, message: "Make sure your password has atleast one number, lowercase and uppercase letter, one symbol and should have a minimum length of 8 characters." }
 end
